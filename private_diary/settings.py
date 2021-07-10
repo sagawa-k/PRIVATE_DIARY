7dd462a89640474db745797ed7af7977a960ea0e
sagawa-k <rain.chap.chap@gmail.com>
2021-07-10 22:37:08 +0900

    gitignore調整

--- private_diary/settings.py
+++ private_diary/settings.py
@@ -21,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--jyzf+z-15qbode5s&yq5he1d(ma*&q)@(3w&#$7fc6x2s+*d&'
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
